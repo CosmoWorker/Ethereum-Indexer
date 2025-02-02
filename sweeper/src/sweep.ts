a4ecbb5f95091f27fbc82e70ecf7269ade7b31d1
CosmoWorker <social.tarang@gmail.com>
2025-02-02 13:15:32 +0000

    decrypting priv keys for sweeping

--- sweeper/src/sweep.ts
+++ sweeper/src/sweep.ts
@@ -2,13 +2,19 @@ import { JsonRpcProvider, parseUnits, Wallet } from "ethers";
 import {PrismaClient} from "../../backend/node_modules/@prisma/client/default"
 import dotenv from "dotenv"
 import axios from "axios";
+import Cryptr from "cryptr";
+
 dotenv.config(); //private key testnet
 const prisma= new PrismaClient(); 
+if(!process.env.ENCRYPT_SECRET_KEY){
+    throw Error("Empty variable");
+}
+const cryptr= new Cryptr(process.env.ENCRYPT_SECRET_KEY);// same secret used in backend, now for decryption
 const MIN_ETH_SWEEP_THRESHOLD=0.05; 
 const maxGasPriceLimit=11;
 const HOT_WALLET_WITHDRAWAL_ADDREES="0x2966473D85A76A190697B5b9b66b769436EFE8e5";
 
-const getGasPrice=async()=>{ //api for gas prices
+const getGasPrice=async()=>{ //api for gas prices estimate
     const res= await axios.get("https://api.blocknative.com/gasprices/blockprices", {
         headers:{ Authorization: process.env.BLOCK_NATIVE_API_KEY},
         params: {chainid: 1, confidenceLevels: "50,70,80,90,99"}
@@ -34,7 +40,7 @@ const getGasPrice=async()=>{ //api for gas prices
         } 
         const gasPriceData=await getGasPrice();
         if(user.balance > MIN_ETH_SWEEP_THRESHOLD && gasPriceData<maxGasPriceLimit){
-            const signer=new Wallet(user.privateKey, provider);
+            const signer=new Wallet(cryptr.decrypt(user.privateKey), provider);
             const txn= await signer.sendTransaction({
                 to: HOT_WALLET_WITHDRAWAL_ADDREES,
                 value: parseUnits(user.balance.toString(), 'ether'),
